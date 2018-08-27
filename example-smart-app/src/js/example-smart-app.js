0d4401f006e60eb21d9f9bdc97800a64830a8282
Ryan Donahue <ryan@wellhive.com>
2018-08-27 10:56:17 -0400

    Try search instead of fetchAll

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,7 +11,7 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
+        var obv = smart.patient.api.search({
                     type: 'Observation',
                     query: {
                       code: {
